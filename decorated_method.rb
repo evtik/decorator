3db3a8b5e7d5774b728b47c12a7c61668cd6ec9a
Serhii <e.v.t.i.k@ukr.net>
2016-11-28 17:14:15 +0200

    Implement decorator

--- decorated_method.rb
+++ decorated_method.rb
@@ -1,3 +1,23 @@
+module Decorators
+  def self.included(klass)
+    klass.extend ClassMethods
+  end
+
+  module ClassMethods
+    def add_prefix(*args)
+      prev_method_added = method :method_added
+
+      define_singleton_method :method_added do |method_name|
+        define_singleton_method :method_added, prev_method_added
+        method_to_decorate = instance_method method_name
+        define_method method_name do
+          args * '' + method_to_decorate.bind(self).call
+        end
+      end
+    end
+  end
+end
+
 class C
   include Decorators
 
